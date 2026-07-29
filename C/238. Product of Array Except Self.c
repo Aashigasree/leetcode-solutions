92ceaa4271f5aa484ae59575c40356b2998b5b12
Aashigasree <aashigasree@gmail.com>
2026-07-29 19:41:09 +0530

    Solved Product of Array Except Self in C

---
+++ C/238. Product of Array Except Self.c
@@ -0,0 +1,21 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int* ans = (int*)malloc(numsSize * sizeof(int));
+    int leftProduct = 1;
+    int rightProduct = 1;
+
+    for (int i = 0; i < numsSize; i++) {
+        ans[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    for (int i = numsSize - 1; i >= 0; i--) {
+        ans[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    *returnSize = numsSize;
+    return ans;
+}
