e2d8857166dc135a57812a10a361df87f90a5456
Aashigasree <aashigasree@gmail.com>
2026-08-15 23:25:53 +0530

    Solved  Find Numbers with Even Number of Digits in C

---
+++ C/1295. Find Numbers with Even Number of Digits.c
@@ -0,0 +1,18 @@
+int findNumbers(int* nums, int numsSize) {
+    int ans = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        int num = nums[i];
+        int count = 0;
+
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+
+        if (count % 2 == 0) {
+            ans++;
+        }
+    }
+    return ans;
+}
