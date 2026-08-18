c4b33f8e91bf02cad0422126e9e3a2641820c86b
Aashigasree <aashigasree@gmail.com>
2026-08-18 22:05:25 +0530

    Solved Monotonic Array in C

---
+++ C/896. Monotonic Array.c
@@ -0,0 +1,17 @@
+bool isMonotonic(int* nums, int numsSize) {
+    int increasing = true;
+    int decreasing = true;
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            increasing = false;
+        }
+        if (nums[i] < nums[i + 1]) {
+            decreasing = false;
+        }
+    }
+    if (increasing || decreasing) {
+        return true;
+    } else {
+        return false;
+    }
+}
