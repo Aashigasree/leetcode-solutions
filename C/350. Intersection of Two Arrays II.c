eaf53be6b71f09623aa132052fd6d733cd00d6df
Aashigasree <aashigasree@gmail.com>
2026-09-20 21:19:02 +0530

    Solved Intersection of Two Arrays II in C

---
+++ C/350. Intersection of Two Arrays II.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size,
+               int* returnSize) {
+    int* result = (int*)malloc(nums1Size * sizeof(int));
+    int k = 0;
+    for (int i = 0; i < nums1Size; i++) {
+        for (int j = 0; j < nums2Size; j++) {
+            if (nums1[i] == nums2[j]) {
+                result[k] = nums1[i];
+                nums1[i] = -1;
+                k++;
+                break;
+            }
+        }
+    }
+    *returnSize = k;
+    return result;
+}
